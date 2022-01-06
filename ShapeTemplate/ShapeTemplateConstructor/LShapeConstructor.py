3138bd32cea4ef9e55772d3a9f6919fe5617f0af
Dawid Dudek <dawid.dudek@fingo.pl>
2022-01-06 17:33:29 +0100

    fix LShapeTemplate

--- ShapeTemplate/ShapeTemplateConstructor/LShapeConstructor.py
+++ ShapeTemplate/ShapeTemplateConstructor/LShapeConstructor.py
@@ -5,28 +5,25 @@ from ShapeTemplate.Field import Field
 from ShapeTemplate.Row import Row
 
 def construct():
-    first_rotate = Box([Row([Field(False), Field(False), Field(False), Field(True)]),
-                        Row([Field(True),  Field(True),  Field(True),  Field(True)]),
-                        Row([Field(False), Field(False), Field(False), Field(False)]),
-                        Row([Field(False), Field(False), Field(False), Field(False)])
+    first_rotate = Box([Row([Field(False), Field(False), Field(True)]),
+                        Row([Field(True),  Field(True),  Field(True)]),
+                        Row([Field(False), Field(False), Field(False)]),
                         ])
 
-    second_rotate = Box([Row([Field(False), Field(True), Field(False), Field(False)]),
-                         Row([Field(False), Field(True), Field(False), Field(False)]),
-                         Row([Field(False), Field(True), Field(False), Field(False)]),
-                         Row([Field(False), Field(True), Field(True), Field(False)])
+    second_rotate = Box([Row([Field(False), Field(True), Field(False)]),
+                         Row([Field(False), Field(True), Field(False)]),
+                         Row([Field(False), Field(True), Field(True)]),
                          ])
 
-    third_rotate = Box([Row([Field(False), Field(False), Field(False), Field(False)]),
-                        Row([Field(False), Field(False), Field(False), Field(False)]),
-                        Row([Field(True),  Field(True),  Field(True),  Field(True)]),
-                        Row([Field(True),  Field(False), Field(False), Field(False)])
+    third_rotate = Box([Row([Field(False), Field(False), Field(False)]),
+                        Row([Field(False), Field(False), Field(False)]),
+                        Row([Field(True),  Field(True),  Field(True)]),
+                        Row([Field(True),  Field(False), Field(False)])
                         ])
 
-    fourth_rotate = Box([Row([Field(True),  Field(True), Field(False), Field(False)]),
-                         Row([Field(False), Field(True), Field(False), Field(False)]),
-                         Row([Field(False), Field(True), Field(False), Field(False)]),
-                         Row([Field(False), Field(True), Field(False), Field(False)])
+    fourth_rotate = Box([Row([Field(True),  Field(True), Field(False)]),
+                         Row([Field(False), Field(True), Field(False)]),
+                         Row([Field(False), Field(True), Field(False)])
                          ])
 
     return ShapeTemplate([first_rotate, second_rotate, third_rotate, fourth_rotate],
