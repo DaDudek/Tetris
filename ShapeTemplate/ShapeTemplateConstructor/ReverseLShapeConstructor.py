5f059312569c5c094fbe9143fa1b33f83d5c7822
Dawid Dudek <dawid.dudek@fingo.pl>
2022-01-06 17:33:36 +0100

    fix ReverseLShapeTemplate

--- ShapeTemplate/ShapeTemplateConstructor/ReverseLShapeConstructor.py
+++ ShapeTemplate/ShapeTemplateConstructor/ReverseLShapeConstructor.py
@@ -6,29 +6,26 @@ from ShapeTemplate.Row import Row
 
 
 def construct():
-    first_rotate = Box([Row([Field(True), Field(False), Field(False), Field(False)]),
-                        Row([Field(True), Field(True), Field(True), Field(True)]),
-                        Row([Field(False), Field(False), Field(False), Field(False)]),
-                        Row([Field(False), Field(False), Field(False), Field(False)])
+    first_rotate = Box([Row([Field(True),  Field(False), Field(False)]),
+                        Row([Field(True),  Field(True),  Field(True)]),
+                        Row([Field(False), Field(False), Field(False)]),
                         ])
 
-    second_rotate = Box([Row([Field(False), Field(True), Field(True), Field(False)]),
-                         Row([Field(False), Field(True), Field(False), Field(False)]),
-                         Row([Field(False), Field(True), Field(False), Field(False)]),
-                         Row([Field(False), Field(True), Field(False), Field(False)])
+    second_rotate = Box([Row([Field(False), Field(True), Field(True)]),
+                         Row([Field(False), Field(True), Field(False)]),
+                         Row([Field(False), Field(True), Field(False)]),
                          ])
 
-    third_rotate = Box([Row([Field(False), Field(False), Field(False), Field(False)]),
-                        Row([Field(False), Field(False), Field(False), Field(False)]),
-                        Row([Field(True),  Field(True),  Field(True),  Field(True)]),
-                        Row([Field(False), Field(False), Field(False), Field(True)])
+    third_rotate = Box([Row([Field(False), Field(False), Field(False)]),
+                        Row([Field(True),  Field(True),  Field(True)]),
+                        Row([Field(False), Field(False), Field(True)])
                         ])
 
-    fourth_rotate = Box([Row([Field(False), Field(True), Field(False), Field(False)]),
-                         Row([Field(False), Field(True), Field(False), Field(False)]),
-                         Row([Field(False), Field(True), Field(False), Field(False)]),
-                         Row([Field(True),  Field(True), Field(False), Field(False)])
+    fourth_rotate = Box([Row([Field(False), Field(True), Field(False)]),
+                         Row([Field(False), Field(True), Field(False)]),
+                         Row([Field(True),  Field(True), Field(False)])
                          ])
+
 
     return ShapeTemplate([first_rotate, second_rotate, third_rotate, fourth_rotate],
                          color.ColorService.get_by_name("BLUE"))
