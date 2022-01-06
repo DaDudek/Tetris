691bd8421addb703e1bca49c9280c4f690b05dce
Dawid Dudek <dawid.dudek@fingo.pl>
2022-01-06 11:13:03 +0100

    first version of model classes

---
+++ model/Point.py
@@ -0,0 +1,17 @@
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def getX(self):
+        return self.x
+
+    def getY(self):
+        return self.y
+
+    def setX(self, x):
+        self.x = x
+
+    def setY(self, y):
+        self.y=y
+
