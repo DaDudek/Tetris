0c178c262b72eae02295765e1acfce35e4c0f4e9
Dawid Dudek <dawid.dudek@fingo.pl>
2022-01-07 10:25:40 +0100

    add simple move left/right mechanism

--- Shape/ShapeMove.py
+++ Shape/ShapeMove.py
@@ -5,3 +5,11 @@ from model.Point import Point
 def move_down(shape):
     shape.set_coordinate(Point(shape.get_coordinate().getX(),
                                shape.get_coordinate().getY() + INIT_SQUARE_SIZE))
+
+def move_left(shape):
+    shape.set_coordinate(Point(shape.get_coordinate().getX()-INIT_SQUARE_SIZE,
+                               shape.get_coordinate().getY()))
+
+def move_right(shape):
+    shape.set_coordinate(Point(shape.get_coordinate().getX()+INIT_SQUARE_SIZE,
+                               shape.get_coordinate().getY()))
