5ce24bf5bc23f85c4eac76526ca1d27a305e7f87
Dawid Dudek <dawid.dudek@fingo.pl>
2022-01-07 19:05:29 +0100

    change destroy row mechanism to prepare for moving down

--- destoryRow/rowChecker.py
+++ destoryRow/rowChecker.py
@@ -2,22 +2,25 @@ from gameSettings.SizeConstants import *
 
 
 def checkForRemove(board):
-    actualRow = board.get_squares()[0].getY()
-    counter = 0
-    possible_needed_to_be_removed = []
-    actually_needed_to_be_removed = []
-    for square in board.get_squares():
-        possible_needed_to_be_removed.append(square)
-        if square.getY() == actualRow:
-            counter += 1
-        else:
-            actualRow = square.getY()
-            counter = 1
-            possible_needed_to_be_removed = [square]
-        if counter == NUMBER_OF_SQUARES_IN_ROW:
-            actually_needed_to_be_removed.extend(possible_needed_to_be_removed)
-            possible_needed_to_be_removed = []
-    board.remove_all_from_list(actually_needed_to_be_removed)
-    print()
-
+    something_was_removed = True
+    while something_was_removed:
+        something_was_removed = False
+        if not board.get_squares():
+            break
+        actual_row = board.get_squares()[0].getY()
+        counter = 0
+        possible_needed_to_be_removed = []
+        for square in board.get_squares():
+            possible_needed_to_be_removed.append(square)
+            if square.getY() == actual_row:
+                counter += 1
+            else:
+                actual_row = square.getY()
+                counter = 1
+                possible_needed_to_be_removed = [square]
+            if counter == NUMBER_OF_SQUARES_IN_ROW:
+                something_was_removed = True
+                break
+        if len(possible_needed_to_be_removed) == NUMBER_OF_SQUARES_IN_ROW:
+            board.remove_all_from_list(possible_needed_to_be_removed)
 
