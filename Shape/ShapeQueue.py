780d2dbe645fa29a5a245220c2d49d06a1a4e133
Dawid Dudek <dawid.dudek@fingo.pl>
2022-01-07 11:39:43 +0100

    change queue to always return different element

--- Shape/ShapeQueue.py
+++ Shape/ShapeQueue.py
@@ -2,9 +2,11 @@ from Shape.Shape import Shape
 from ShapeTemplate.ShapeTemplateConstructor.ShapeTemplateConstructor import construct_by_id
 import random
 
+
 class ShapeQueue:
     def __init__(self):
-        self.queue = []  #shape queue
+        self.queue = []  # shape queue
+        self.last_index = 0
         for i in range(4):
             self.add_random_to_queue()
 
@@ -15,9 +17,17 @@ class ShapeQueue:
         self.queue.append(shape)
 
     def add_random_to_queue(self):
-        random_template = construct_by_id(random.randint(0, 6))
+        self.choose_different_then_previous()
+        random_template = construct_by_id(self.last_index)
         self.add_to_queue(Shape(random_template))
 
     def remove_current(self):
         self.queue.pop(0)
 
+    def choose_different_then_previous(self):
+        possible_indexes = [x for x in range(7)]
+        possible_indexes.remove(self.last_index)
+        random_id = random.choice(possible_indexes)
+        possible_indexes.append(self.last_index)
+        possible_indexes.remove(random_id)
+        self.last_index = random_id
