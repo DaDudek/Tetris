0f5c0a5c5d00fe4897e9f9ee30be9e2913b7ae96
Dawid Dudek <dawid.dudek@fingo.pl>
2022-01-07 11:27:01 +0100

    add dummy collision detector

--- Shape/Move/CollisionDetector.py
+++ Shape/Move/CollisionDetector.py
@@ -1,16 +1,15 @@
 from gameSettings.SizeConstants import *
 
 
-def check_for_collision(shape):
+def check_for_vertical_collision(shape):
     for point in shape.get_points():
-        if check_for_vertical_collision(point) or check_for_horizontal_collision(point):
+        if point.getY() >= INIT_HIGHT:
             return True
     return False
 
 
-def check_for_vertical_collision(point):
-    return point.getY() >= INIT_HIGHT
-
-
-def check_for_horizontal_collision(point):
-    return point.getX() >= INIT_WIGHT or point.getX <= 0
+def check_for_horizontal_collision(shape):
+    for point in shape.get_points():
+        if point.getX() >= INIT_WIGHT or point.getX <= 0:
+            return True
+    return False
